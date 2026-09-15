c310f5776f24d57d0dd989e8f0d15aca28644bdb
VanHoang0612 <nguyenvanhoang031206@gmail.com>
2026-09-15 13:01:35 +0700

    Initialize project

---
+++ src/main/java/com/example/hcnstore_backend/HcnstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hcnstore_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcnstoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcnstoreBackendApplication.class, args);
+    }
+
+}
